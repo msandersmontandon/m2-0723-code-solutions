875422206df14a3c24ea2ad3800e0b386eeab59a
Matheus Sanders Montandon <msmontandon@gmail.com>
2023-07-21 04:33:07 +0000

    Changes to for-each.js

--- array-methods/for-each.js
+++ array-methods/for-each.js
@@ -3,6 +3,5 @@ const values = [10, 1, 22, 23, 41, 5, 18, 7, 80, 9];
 console.log('In order:');
 values.forEach(element => {console.log(element)});
 
-let index = 0;
 console.log('Reverse order:');
-values.forEach(() => {console.log(values[values.length - ++index])});
+values.forEach((element, index) => {console.log(values[values.length - index - 1])});
